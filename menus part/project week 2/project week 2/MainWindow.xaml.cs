a1455f91bad5a3ce9713e06b9dd492e27453914e
Devilskey <501068@vistacollege.nl>
2022-01-25 14:47:54 +0100

    menu parts added

---
+++ menus part/project week 2/project week 2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_week_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            aboutpage ap = new aboutpage();
+            ap.Show();
+            this.Close();
+        }
+
+        private void guid_Click(object sender, RoutedEventArgs e)
+        {
+            guide g = new guide();
+            g.Show();
+            this.Close();
+        }
+
+    }
+}
---
+++ menus part/project week 2/project week 2/Vraag.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_week_2
+{
+    /// <summary>
+    /// Interaction logic for Vraag.xaml
+    /// </summary>
+    public partial class Vraag : Window
+    {
+        public Vraag()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            Vraag v = new Vraag();
+            v.Show();
+            this.Close();
+        }
+
+        private void guide_Click(object sender, RoutedEventArgs e)
+        {
+            guide g = new guide();
+            g.Show();
+            this.Close();
+        }
+    }
+}
